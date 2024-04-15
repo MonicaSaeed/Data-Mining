78449492beb77c100a400bf7a1f44ed14379a303
Monica Saeed <monicasaeed12@gmail.com>
2024-04-15 15:57:25 +0200

    .

--- Assignment2/movie/movies.py
+++ Assignment2/movie/movies.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-def read_data(persentage):
+def read_data(percentage):
     num_rows = 0
     with open('imdb_top_2000_movies.csv') as f:
         num_rows = sum(1 for line in f)
-    num_read_rows = int(num_rows * persentage / 100)
+    num_read_rows = int(num_rows * percentage / 100)
     data = pd.read_csv('imdb_top_2000_movies.csv', usecols=['Movie Name', 'IMDB Rating'], nrows=num_read_rows)
     return data
 
